54db04ff44cbf0764fa49fee60f0d1e22faaf7c2
StakanAgan <aganek9611@gmail.com>
2019-01-30 11:56:34 +0300

    changed utils

--- utils.py
+++ utils.py
@@ -1,16 +1,15 @@
 import random
 import string
 import paramiko
+import config
 
 def connect_vpn(): #### подключение к серверу по ssh
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, password=secret, port=port)
-    stdin, stdout, stderr = client.exec_command('ls')
-    data = stdout.read() + stderr.read()
-    print(data[:10])
-    client.close()
-    return data
+    client.connect(hostname=config.host, username=config.user, password=config.secret, port=config.port)
+    channel = client.get_transport().open_session()
+    channel.get_pty()
+    channel.settimeout(5)
 
 def local_connect(): #### локальное подключение к станции
     client = paramiko.SSHClient()
