eb9bcbe1b81f453405047b8228add5c9a07395cb
StakanAgan <aganek9611@gmail.com>
2019-01-30 13:57:54 +0300

    global Pasha

--- utils.py
+++ utils.py
@@ -4,10 +4,10 @@ import paramiko
 import config
 
 def connect_vpn(): #### подключение к серверу по ssh
-    client = paramiko.SSHClient()
+    global client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=config.host, username=config.user, password=config.secret, port=config.port)
-    channel = client.get_transport().open_session()
+    global channel = client.get_transport().open_session()
     channel.get_pty()
     channel.settimeout(5)
 
